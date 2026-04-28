3a6795c00da3fef957b350b6378742239a074c75
uliboooo <uliboulibo@gmail.com>
2026-04-28 21:46:10 +0900

    update info & version fmt in footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import pkg from './package.json';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static'
+  output: 'static',
+  vite: {
+    define: {
+      __APP_VERSION__: JSON.stringify(pkg.version),
+    },
+  },
 });
